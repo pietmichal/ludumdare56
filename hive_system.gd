c2041535f8ff6c73ab70fc94ee2995ea490b79da
pietmichal <4310372+pietmichal@users.noreply.github.com>
2024-10-08 22:09:08 +0100

    fps fix

--- hive_system.gd
+++ hive_system.gd
@@ -6,13 +6,14 @@ signal tc_bite()
 var tinyCreatureScene = preload("res://tiny_creature.tscn")
 
 var bitesLeft = 3
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.aa
 func _ready() -> void:
 	Hive.tc_bite.connect(on_bite)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var creatures = get_tree().get_nodes_in_group("TinyCreatures") as Array[TinyCreature]
 	# debug stuff
 	if Input.is_action_just_pressed("spawnTC"): 
 		for i in range(0, 10):
@@ -30,6 +31,8 @@ func on_bite() -> void:
 	
 func spawn_tiny_creature() -> void:	
 	var creatures = get_tree().get_nodes_in_group("TinyCreatures") as Array[TinyCreature]
+	
+	if creatures.size() > 500: return
 	
 	var instance = tinyCreatureScene.instantiate()
 	
